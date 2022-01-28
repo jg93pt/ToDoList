f898dc433d00b159cd2853a394023983db43b558
jg93pt <jorgebenf@gmail.com>
2022-01-28 13:11:18 +0000

    Created Marked Tasks Active and Complete + toggle buttons All, Active, Completed

--- js/application.js
+++ js/application.js
@@ -11,11 +11,20 @@ var getAllTasks = function () {
 
       response.tasks.forEach(function (task) {
         // Show in html the result:
+        if (task.completed) {
     $('tbody').prepend('<tr class="spacetable">' + 
+    '<td class="task">' + task.content + '</td>' +
+    '<td><input class="form-check-input" type="checkbox"  data-id="'+ task.id +'" checked></td>' +
+    '<td><button class="btn btn-danger btn-remove" id ="removeTask" data-id ="' + task.id + '" >Remove</button></td>'+ 
+    '</tr>');
+        }
+        else if (!task.completed) {
+          $('tbody').prepend('<tr class="spacetable">' + 
     '<td class="task">' + task.content + '</td>' +
     '<td><input class="form-check-input" type="checkbox"  data-id="'+ task.id +'"></td>' +
     '<td><button class="btn btn-danger btn-remove" id ="removeTask" data-id ="' + task.id + '" >Remove</button></td>'+ 
     '</tr>');
+        }
       });
     },
     error: function (request, textStatus, errorMessage) {
@@ -69,7 +78,104 @@ $(document).on('click', '#removeTask', function () {
   deleteTask($(this).data('id'));
 });
 
-// Calling GET function
+//Mark TASK Complete
+var MarkTaskComplete = function (id) {
+  $.ajax({
+    type: 'PUT',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id +'/mark_complete?api_key=223',
+      dataType: 'json',
+      success: function (response, textStatus) {
+        getAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+}
+
+// Mark TASK Active
+var MarkTaskActive = function (id) {
+  $.ajax({
+    type: 'PUT',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id +'/mark_active?api_key=223',
+      dataType: 'json',
+      success: function (response, textStatus) {
+        getAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+}
+//Mark TASK checkbox function
+$(document).on('change', '.form-check-input', function () {
+  if (this.checked) {
+    MarkTaskComplete($(this).data('id'));
+  } else {
+    MarkTaskActive($(this).data('id'));
+  }
+});
+
+// All marked function
+$(document).on('click', '#All', function() {
+  getAllTasks();
+});
+
+// GET Active tasks function
+var GetMarkActive = function () {
+  $.ajax({
+    type: 'GET',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=223',
+      dataType: 'json',
+      success: function (response, textStatus) {
+        $('#todo-list').empty(); 
+        response.tasks.forEach(function (task) {
+        if(!task.completed) {
+          $('tbody').prepend('<tr class="spacetable">' + 
+          '<td class="task">' + task.content + '</td>' +
+          '<td><input class="form-check-input" type="checkbox"  data-id="'+ task.id +'"></td>' +
+          '<td><button class="btn btn-danger btn-remove" id ="removeTask" data-id ="' + task.id + '" >Remove</button></td>'+ 
+          '</tr>');
+        }
+      });
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+}
+$(document).on('click', '#Active', function() {
+  GetMarkActive();
+});
+
+// GET Complete tasks function
+var GetMarkComplete = function () {
+  $.ajax({
+    type: 'GET',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=223',
+      dataType: 'json',
+      success: function (response, textStatus) {
+        $('#todo-list').empty(); 
+        response.tasks.forEach(function (task) {
+        if(task.completed) {
+          $('tbody').prepend('<tr class="spacetable">' + 
+          '<td class="task">' + task.content + '</td>' +
+          '<td><input class="form-check-input" type="checkbox"  data-id="'+ task.id +'" checked></td>' +
+          '<td><button class="btn btn-danger btn-remove" id ="removeTask" data-id ="' + task.id + '" >Remove</button></td>'+ 
+          '</tr>');
+        }
+      });
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+}
+$(document).on('click', '#Completed', function() {
+  GetMarkComplete();
+});
+
+// Calling GET function at ready
 getAllTasks();
 });
 
